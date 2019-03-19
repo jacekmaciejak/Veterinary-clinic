f4437b98093bb46825a631f6b965d0cf3eded1b0
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-03-19 15:08:26 +0100

    Added script to new file

---
+++ scripts/close_menu.js
@@ -0,0 +1,20 @@
+/*JavaScript Function closing menu after click on menu item/link */
+
+(function () {
+	function uncheckNav() {
+		document.getElementById("navi-toggle").checked = false;
+	}
+
+	function setNavOnclick() {
+		Array.from(
+				document.getElementsByClassName("navigation__link")
+			)
+			.map(navLink => navLink.addEventListener("click", uncheckNav));
+	}
+
+	function main() {
+		setNavOnclick();
+	}
+
+	main();
+})()
